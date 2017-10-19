479af9e624b4317e8c9ec4c6ba240c3df9db63fc
RameezShuhaib <rameezshuhaib96@gmail.com>
2017-10-19 13:30:05 +0530

    ionic design src

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { ProfilePage } from '../pages/profile/profile';
+import { AttendancePage } from '../pages/attendance/attendance';
+import { MarksPage } from '../pages/marks/marks';
+import { TabsControllerPage } from '../pages/tabs-controller/tabs-controller';
+import { LoginPage } from '../pages/login/login';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    ProfilePage,
+    AttendancePage,
+    MarksPage,
+    TabsControllerPage,
+    LoginPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    ProfilePage,
+    AttendancePage,
+    MarksPage,
+    TabsControllerPage,
+    LoginPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
