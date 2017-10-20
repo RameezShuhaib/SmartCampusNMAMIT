5611357b3f6356d8c31ceedaecb6ad7a34673436
RameezShuhaib <rameezshuhaib96@gmail.com>
2017-10-20 17:04:28 +0530

    done with login

--- SmartCampusServer/app.js
+++ SmartCampusServer/app.js
@@ -16,12 +16,28 @@ app.get('/', function(req,res) {
 });
 
 app.post('/login', function(req,res){
-    if (db.query.login(req.body.usn, req.body.pass)){
-        res.json({Success:true})
+    login = db.query.login(req.body.usn, req.body.pass)
+    if (login){
+        login['Success'] = true
+        res.json(login)
     }else{
         res.json({Success:false})
     }
 });
+
+app.get('/getProfile', function(req,res){
+    res.json(db.query.getProfile(req.body.usn))
+});
+
+app.get('/getMarks', function(req,res){
+    res.json(db.query.getProfile(req.body.exam))
+});
+
+app.get('/getAttendence', function(req,res){
+    res.json(db.query.getAttendence(req.body.sem))
+});
+
+
 
 app.listen(port, function(){
     console.log("Server started on port "+port);
--- SmartCampusServer/db.js
+++ SmartCampusServer/db.js
@@ -6,10 +6,23 @@ module.exports = {
     Connection: con,
     query: {
         login: function(usn, pass){
-            if (usn == '4nm14cs128' && pass == '123')
-                return true;
-            else
-                return false;
+            q = "SELECT s.sid,s.stdname,s.University_Seat,s.semester, s.course, s.division, s.cycle, s.branch, c.course AS crs, b.branch AS brnch, c.have_cycles FROM studentdetails s JOIN course c ON s.course = c.uid JOIN branches b ON s.branch = b.uid AND University_Seat='" + usn + "' and DOB='" + pass + "'";
+            con.query(q, function(err, result, fields) {
+                if (err) throw err;
+                if(result.length > 0)
+                    return result
+                else
+                    return false
+            });
+        },
+        getProfile: function(usn){
+
+        },
+        getMarks: function(exam){
+
+        },
+        getAttendence: function(sem){
+
         }
     }
 }
