94ef79f227ecf5a34deabdf2a148cd3577b4cac4
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2017-10-21 17:20:22 +0530

    Removed Pipes

--- smartCampusStudent/src/pipes/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { UserProfilePipe } from './user-profile/user-profile';
-@NgModule({
-	declarations: [UserProfilePipe],
-	imports: [],
-	exports: [UserProfilePipe]
-})
-export class PipesModule {}
