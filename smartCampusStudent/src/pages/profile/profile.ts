c6494880cf68e479318f97bbfaf702ae66373b70
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2017-10-21 15:18:26 +0530

    Modified profile.js

--- smartCampusStudent/src/pages/profile/profile.ts
+++ smartCampusStudent/src/pages/profile/profile.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-profile',
-  templateUrl: 'profile.html',
+  templateUrl: 'profile.html'
 })
 
 export class ProfilePage{
@@ -13,10 +13,14 @@ export class ProfilePage{
   // should be each tab's root Page
 
   profile: any;
+  usn:String;
   constructor(public navCtrl: NavController, public http:Http) {
   }
+
+
   ionViewWillEnter() {
-    this.http.get('http://localhost:3000/getProfile?usn=4NM12ME003')
+    this.usn = '4NM12ME003';
+    this.http.get(`http://localhost:3000/getProfile?usn=${this.usn}`)
     .map(res => res.json())
     .subscribe((data) => {
         this.profile = data[0];
